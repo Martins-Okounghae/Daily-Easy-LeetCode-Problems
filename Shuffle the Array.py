734c50d382ccbd837fce71c70b557bc440406e6b
Martins-Okuonghae <martins2018.okuonghae@outlook.com>
2021-10-15 00:28:09 +0100

    Shuffle Array problem solved

---
+++ Shuffle the Array.py
@@ -0,0 +1,13 @@
+#Shuffle the Array
+import itertools
+def shuffle_Array(nums, n):
+    new_Array = []
+
+    for num in range(n):
+        new_Array.append(nums[num::n])
+    return list(itertools.chain.from_iterable(new_Array))
+
+
+result = shuffle_Array([1,2,3,4,4,3,2,1], n = 4)
+
+print(result)
