d0c4cfb7779b7d95519d00302c5df15087b183e8
Martins-Okuonghae <martins2018.okuonghae@outlook.com>
2021-10-14 01:39:54 +0100

    Most Frequent problem solved

--- Permutation array.py
+++ Permutation array.py
@@ -1,10 +1,10 @@
 def buildArray(nums):
-    return ([nums[nums[i]] for i in range(len(nums))])
+    return[nums[nums[i]] for i in range(len(nums))]
 
 
+result = buildArray([2,6,5,1,3,2,4])
 
-result = buildArray([1,2,3,5,4,0])
-
-# Newresult = [2,3,5,0,4,1]
+#newresult = [5,4,2,6,1,5,3]
 
 print(result)
+
